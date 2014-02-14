0f2c3d5525ea31875dbfb9465e895b3d34d93bef
Ryan Dwyer <ryanpdwyer@gmail.com>
2014-02-14 07:38:04 -0500

    Added a function to make the date always monday

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+import datetime as dt
 
 
 def restructured_title(string, title_char):
@@ -7,3 +8,13 @@ def restructured_title(string, title_char):
 
 def make_text_date(date):
     return date.strftime("%A, %B %d")
+
+
+def make_date_monday(date):
+    tdelta = dt.timedelta(date.weekday())
+    return date - tdelta
+
+
+class Formatter(object):
+    def __init__(self, year, month, day):
+        self.date = make_date_monday(dt.date(year, month, day))
--- tests.py
+++ tests.py
@@ -38,8 +38,9 @@ Friday, February 14
 
 """
 
-from __init__ import restructured_title, make_text_date
-import datetime
+from __init__ import (restructured_title, make_text_date, Formatter,
+                      make_date_monday)
+import datetime as dt
 from nose.tools import eq_
 
 
@@ -48,8 +49,60 @@ def test_restructured_title():
 
 
 def test_make_text_date():
-    eq_(make_text_date(datetime.date(2014, 2, 10)), "Monday, February 10")
+    eq_(make_text_date(dt.date(2014, 2, 10)), "Monday, February 10")
 
+
+def test_make_date_monday():
+    days = [dt.date(2014, 2, 10) + dt.timedelta(i) for i in xrange(7)]
+    exp_results = [dt.date(2014, 2, 10)] * 7
+    results = [make_date_monday(day) for day in days]
+    for exp, r in zip(exp_results, results):
+        eq_(exp, r)
+
+
+
+# def testFormatter():
+#     exp_result = """
+# 2014 February 10-16
+# ###################
+
+# :author: Ryan Dwyer
+# :date: 2014-02-10
+# :modified: 2014-02-10
+# :subtitle:  Weekly Summary
+# :tags: summary
+
+# Weekly Summary
+# ==============
+
+
+# Daily Summaries
+# ===============
+
+
+# Monday, February 10
+# -------------------
+
+
+# Tuesday, February 11
+# --------------------
+
+
+# Wednesday, February 12
+# ----------------------
+
+
+# Thursday, February 13
+# ---------------------
+
+
+# Friday, February 14
+# -------------------
+
+
+# """
+#     f = Formatter(2014, 2, 10)
+#     eq_(exp_result, f.format())
 
 """
 A title
@@ -72,4 +125,14 @@ Weekly Summary
 
 Daily Summaries
 ===============
+
+{self.monday}
+
+{self.tuesday}
+
+{self.wednesday}
+
+{self.thursday}
+
+{self.friday}
 """
